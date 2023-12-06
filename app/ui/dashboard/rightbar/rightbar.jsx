0cba08bb5a8905664031e9aceb3dedc02d9fafee
kenbadertscher <badertscher.ken@gmail.com>
2023-12-05 20:26:59 -0500

    Rightbar work

--- app/ui/dashboard/rightbar/rightbar.jsx
+++ app/ui/dashboard/rightbar/rightbar.jsx
@@ -1,9 +1,44 @@
 import React from 'react'
+import styles from "./rightbar.module.css"
+import Image from "next/image"
+import { MdPlayCircleFilled, MdReadMore } from "react-icons/md"
 
 const Rightbar = () => {
   return (
-	<div>
-	  Rightbar
+	<div className={styles.container}>
+		<div className={styles.item}>
+			<div className={styles.bgContainer}>
+				<Image src="/astronaut.png" alt="" className={styles.bg} fill />
+			</div>
+			<div className={styles.texts}>
+				<span className={styles.notification}>Available Now</span>
+				<h3 className={styles.title}>How to use the new version of the admin dashboard?</h3>
+				<span className={styles.subtitle}>Takes 4 minutes to learn</span>
+				<p className={styles.desc}>
+					Lorem ipsum dolor sit amet consectetur adipiscing elit.
+					Reprehenderit eius libero perspiciatis recusandae possimus.
+				</p>
+				<button className={styles.button}>
+					<MdPlayCircleFilled />
+					Watch
+				</button>
+			</div>
+		</div>
+		<div className={styles.item}>
+			<div className={styles.texts}>
+				<span className={styles.notification}>Coming Soon</span>
+				<h3 className={styles.title}>New server actions are avialable, partial pre-rendering is coming up!</h3>
+				<span className={styles.subtitle}>Boost your productivity</span>
+				<p className={styles.desc}>
+					Lorem ipsum dolor sit amet consectetur adipiscing elit.
+					Reprehenderit eius libero perspiciatis recusandae possimus.
+				</p>
+				<button className={styles.button}>
+					<MdReadMore />
+					Learn
+				</button>
+			</div>
+		</div>
 	</div>
   )
 }
